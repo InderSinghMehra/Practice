3c802ff03f00ffb9813c0c3678839935c322553c
GenSpock <genspock@Ubuntu-18.04>
2020-02-01 09:44:10 +0530

    second add

---
+++ src/DemoIfElse.scala
@@ -0,0 +1,22 @@
+
+
+object DemoIfElse {
+  def main(args: Array[String]) {
+   val x = 20;
+   val y = 30;
+   var res ="";
+   
+   if(x == 20 && y == 30){    //&& both condition  || one condition true will do the work.
+     println("It macthes")
+     res = "x == 20 && y == 30";
+   } else {
+     println("No it doesn't")
+     res = "x != 20";
+   }
+   
+     println(res);
+     
+     val res2 = if (x != 20) "x == 20" else "x != 20";
+     println(res2); 
+  }
+}
